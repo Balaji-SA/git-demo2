4d7e6319230be564929b4047d089e87c3c075a2b
Balaji SA <50614674+Balaji-SA@users.noreply.github.com>
2021-03-10 13:42:37 +0530

    Update Jenkins.java

--- Jenkins.java
+++ Jenkins.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 class Jenkins{
-  public static void main(String args[]))
+  public static void main(String args[])
   {
     System.out.println("Hello world changed");
   }
